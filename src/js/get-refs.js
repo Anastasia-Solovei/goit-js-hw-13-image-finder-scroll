7c964f8628fbcc7a5867e95e7cb12b446e86cf6f
Anastasia Solovei <av.ignatyuk@gmail.com>
2021-06-15 17:16:54 +0300

    deletes refs of load-more btn, scroll elem and container, adds ref of js-scroll-container

--- src/js/get-refs.js
+++ src/js/get-refs.js
@@ -2,8 +2,6 @@ export default function getRefs() {
   return {
     searchForm: document.querySelector('#search-form'),
     galleryContainer: document.querySelector('.js-gallery'),
-    loadMoreBtn: document.querySelector('button[data-action="load-more"]'),
-    scrollElem: document.querySelector('.scroll-elem'),
-    // container: document.querySelector('.container'),
+    scrollContainer: document.querySelector('.js-scroll-container'),
   };
 }
